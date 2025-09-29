c28f96df34ffedad06a7472af8ca2b637559a1c4
Leonardo2397 <leonardoferrante753@gmail.com>
2025-09-29 16:35:04 +0200

    edit

--- src/main/java/amedeo/mignano/Application.java
+++ src/main/java/amedeo/mignano/Application.java
@@ -1,14 +1,25 @@
 package amedeo.mignano;
 
+import amedeo.mignano.dao.VenditoreDAO;
+import amedeo.mignano.entities.Rivenditore;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.util.UUID;
 
 public class Application {
 
     public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4pu");
     public static void main(String[] args) {
-        EntityManager em = emf.createEntityManager();
-        System.out.println("Hello World!");
+        var em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(new Rivenditore(UUID.randomUUID()));
+        em.getTransaction().commit();
+        em.close();
+
+
+        VenditoreDAO dao = new VenditoreDAO();
+        dao.menu();
     }
 }
