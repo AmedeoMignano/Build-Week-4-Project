1c1ba66fb9462016ff6675352a86120b58858143
AmedeoMignano <amedeo56@gmail.com>
2025-09-29 12:01:03 +0200

    Adds Emf

--- src/main/java/amedeo/mignano/Application.java
+++ src/main/java/amedeo/mignano/Application.java
@@ -1,8 +1,14 @@
 package amedeo.mignano;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+    public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4pu");
     public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
         System.out.println("Hello World!");
     }
 }
