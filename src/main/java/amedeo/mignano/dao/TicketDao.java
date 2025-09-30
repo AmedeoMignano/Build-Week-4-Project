e799d064f1b6446e05cfef9cde7bd7a8c1a9a0ce
Leonardo2397 <leonardoferrante753@gmail.com>
2025-09-30 13:04:16 +0200

    edit

--- src/main/java/amedeo/mignano/dao/TicketDao.java
+++ src/main/java/amedeo/mignano/dao/TicketDao.java
@@ -3,11 +3,17 @@ package amedeo.mignano.dao;
 import amedeo.mignano.entities.Ticket;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
 
 public class TicketDao {
     private final EntityManager em;
 
     public TicketDao(EntityManager em){
+
         this.em = em;
     }
 
@@ -16,5 +22,31 @@ public class TicketDao {
         tr.begin();
         em.persist(ticket);
         tr.commit();
+    }
+
+    // counter per biglietti in determinato intervallo di tempo
+    public long countByPeriodo(LocalDate start, LocalDate end) {
+        TypedQuery<Long> query = em.createQuery(
+                "SELECT COuNT (t) FROM Ticket t WHERE t.datavendita BETWEEN :start and :end", Long.class);
+
+        query.setParameter("start", start);
+        query.setParameter("end", end);
+        return query.getSingleResult();
+    }
+
+    //counter biblietti stesso intervallo ma di un solo venditore
+    public List<Object[]> countPerVenditore(LocalDate start,LocalDate end) {
+        TypedQuery<Object[]> query = em.createQuery(
+                "SELECT v, COUNT(t) " +
+                        "FROM Ticket t JOIN t.venditore v " +
+                        "WHERE t.dataVendita BETWEEN :start AND :end" +
+                        "GROUP BY v",
+                Object[].class
+        );
+
+        query.setParameter("start", start);
+        query.setParameter("end", end);
+        return query.getResultList();
+
     }
 }
