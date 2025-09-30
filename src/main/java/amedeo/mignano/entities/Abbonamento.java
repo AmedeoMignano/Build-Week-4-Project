1da6a0ff97e78beed1688a0a422c42f9aa323d15
AmedeoMignano <amedeo56@gmail.com>
2025-09-30 08:22:25 +0200

    Adds En

--- src/main/java/amedeo/mignano/entities/Abbonamento.java
+++ src/main/java/amedeo/mignano/entities/Abbonamento.java
@@ -1,10 +1,7 @@
 package amedeo.mignano.entities;
 
 import amedeo.mignano.entities.enums.Tipologia;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import javax.smartcardio.Card;
 import java.time.LocalDate;
@@ -12,6 +9,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "abbonamenti")
 public class Abbonamento extends Ticket{
+    @Enumerated(EnumType.STRING)
     private Tipologia tipologia;
     @ManyToOne
     @JoinColumn(name = "card_id")
