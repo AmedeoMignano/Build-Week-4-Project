a0e67b6dde9da711171d106733e1b22d3427718e
Anna De Lise <anna_delise@libero.it>
2025-10-01 16:27:09 +0200

    add fix

--- src/main/java/amedeo/mignano/Application.java
+++ src/main/java/amedeo/mignano/Application.java
@@ -39,7 +39,7 @@ while (true) {
         int scelta = Integer.parseInt(scanner.nextLine());
         switch (scelta) {
             case 1 :
-                //ADMIN
+                menuAdmin();
                 break;
             case 2 :
                 menuUtente();
@@ -56,12 +56,8 @@ while (true) {
 
     }
 }
-
-
-
     }
-
-
+    
     // Metodi dei menu e delle creazioni
 
     public static void ticketMenu() {
@@ -387,7 +383,104 @@ while (true) {
            System.out.println(e.getMessage());
        }
     }
+    public static void creaMezzo(StatoMezzoTrasportoDAO std) {
+        TipoMezzoTrasporto tipo;
+        try {
+            System.out.println("Inserisci TIPOLOGIA MEZZO: AUTOBUS / TRAM");
+            String input = scanner.nextLine().toUpperCase().trim();
+            if (input.equals("AUTOBUS") || input.equals("BUS"))
+                tipo = TipoMezzoTrasporto.AUTOBUS;
+            else if (input.equals("TRAM")) {
+                tipo = TipoMezzoTrasporto.TRAM;
+            } else {
+                throw new InputErratoException("INPUT ERRATO! SOLO TRAM O BUS!");
+            }
+        } catch (NumberFormatException | InputErratoException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+        MezzoTrasporto mt = new MezzoTrasporto(tipo);
+        mtd.creaSalva(std, mt);
+        System.out.println("Mezzo salvato in DB!\nId: " + mt.getId());
+    }
 
+    public static void updateStatoMezzo( StatoMezzoTrasportoDAO std){
+        try {
+            System.out.println("Inserisci ID mezzo di trasporto:");
+            int mezzoId;
+            try {
+                mezzoId = Integer.parseInt(scanner.nextLine());
+                if (mezzoId <= 0) {
+                    throw new NumberFormatException("ID non valido");
+                }
+            } catch (NumberFormatException e) {
+                throw new InputErratoException("INSERISCI SOLO NUMERI");
+            }
+            System.out.println("Inserisci nuovo stato: IN_SERVIZIO / FUORI_SERVIZIO / IN_MANUTENZIONE");
+            String inputStato = scanner.nextLine().toUpperCase();
+            Stato nuovoStato;
+            if (inputStato.equals("IN_SERVIZIO") || inputStato.equals("INSERVIZIO") || inputStato.equals("IN SERVIZIO")) {
+                nuovoStato = Stato.IN_SERVIZIO;
+            } else if (inputStato.equals("FUORI_SERVIZIO") || inputStato.equals("FUORISERVIZIO") || inputStato.equals("FUORI SERVIZIO")) {
+                nuovoStato = Stato.FUORI_SERVIZIO;
+            } else if (inputStato.equals("IN_MANUTENZIONE") || inputStato.equals("INMANUTENZIONE") || inputStato.equals("IN MANUTENZIONE")) {
+                nuovoStato = Stato.IN_MANUTENZIONE;
+            } else {
+                throw new InputErratoException("STATO NON VALIDO! SOLO: IN_SERVIZIO, FUORI_SERVIZIO o IN_MANUTENZIONE");
+            }
+            mtd.updateStato(std,mezzoId, nuovoStato);
+        } catch (InputErratoException | ElementoNonTrovatoException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void creaTratta() {
+        try {
+            System.out.println("Inserisci PARTENZA");
+            String partenza = scanner.nextLine();
+            System.out.println("Inserisci CAPOLINEA");
+            String capolinea = scanner.nextLine();
+            System.out.println("Inserisci TEMPO PERCORRENZA PREVISTO");
+            String tempoInput = scanner.nextLine();
+            double tempoPercorrenzaPrevisto;
+            try {
+                tempoPercorrenzaPrevisto = Double.parseDouble(tempoInput);
+                if (tempoPercorrenzaPrevisto <= 0) {
+                    throw new NumberFormatException("INPUT NON VALIDO");
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println(ex.getMessage());
+                return;
+            }
+            td.creaSalva(partenza,capolinea,tempoPercorrenzaPrevisto);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void menuAdmin() {
+        boolean running = true;
+        while (running) {
+            System.out.println("\n--- Menu Admin ---");
+            System.out.println("1. Aggiungi mezzo di trasporto");
+            System.out.println("2. Update stato mezzo di trasporto");
+            System.out.println("3. Inserisci nuova tratta");
+            System.out.println("0. Torna indietro");
+            System.out.print("Scelta: ");
+            String scelta = scanner.nextLine();
+            switch (scelta) {
+                case "1" -> {
+                    creaMezzo(smtd);
+                }
+                case "2" -> {
+                    updateStatoMezzo(smtd);
+                }
+                case "3" ->  creaTratta();
+                case "0" -> running = false;
+                default -> System.out.println("Scelta non valida, riprova.");
+            }
+        }
+    }
 
 }
 
